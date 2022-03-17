77bda52c1c76fa87809556b81beb9f3c10546e34
isaacwach <isaac.ndirangu@student.moringaschool.com>
2022-03-17 22:21:38 +0300

    form validation

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,9 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-
-  constructor() { }
-
+  
+  constructor() { 
+  }
+  
   ngOnInit(): void {
   }
 
